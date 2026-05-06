eb5d4de5d9a225fd169c040c63676b3e768c52c1
lady_han <drhandiseni@gmail.com>
2026-05-06 12:13:15 +0200

    Initial Commit

---
+++ variables/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  variables
+//
+//  Created by Purity Handiseni on 6/5/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ variables/variablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  variablesApp.swift
+//  variables
+//
+//  Created by Purity Handiseni on 6/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct variablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
